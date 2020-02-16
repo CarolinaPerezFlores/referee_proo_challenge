9749b92db94630dabed82f950dff235980961455
karo0wk <karo0wk@gmail.com>
2020-02-16 09:43:01 -0600

    the style sheet is categorized

---
+++ src/components/Page.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+
+function Page() {
+  return (
+    <div>
+      <header >
+        <p>Holi crayoli</p> 
+      </header>
+    </div>
+  );
+}
+
+export default Page;
+
+
+
